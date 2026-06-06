defdd8862070e59893c57ba9a936456972b4709d
ttterabyte <teodorocosta2008@gmail.com>
2026-06-06 10:53:52 -0300

    magic 8 ball: ask anything!

---
+++ magic8.py
@@ -0,0 +1,23 @@
+question = input('Question: ')
+
+import random
+num = random.randint(1, 9)
+
+if num == 1:
+  print('Yes - definitely.')
+elif num == 2:
+  print('It is decidedly so.')
+elif num == 3:
+  print('Whitout a doubt.')
+elif num == 4:
+  print('Reply hazy, try again')
+elif num == 5:
+  print('Ask again later.')
+elif num == 6:
+  print('Better not tell you now.')
+elif num == 7:
+  print('My sources say no.')
+elif num == 8:
+  print('Outlook not so good')
+else:
+  print('Very doubtful.')
