a2fb514d5fcda488393a663499c8a31988146e6f
ttterabyte <teodorocosta2008@gmail.com>
2026-06-16 16:01:43 -0300

    first list utility

---
+++ listpractice.py
@@ -0,0 +1,13 @@
+todo = [
+    'wake up',
+    'school',
+    'gymmmm',
+    'study',
+    'study more',
+    'eat.',
+    'capoeira ne papai',
+    'sleep'
+]
+print(todo[0])
+print(todo[1])
+print(todo[2 : 5])
